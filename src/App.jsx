4380fdcf9750841179469002ba9fbd5227e0b569
Brian2544 <95779017+Brian2544@users.noreply.github.com>
2024-12-09 13:30:03 +0300

    Add updates to index, components, styles, and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import HomePage from "./routes/homePage/homePage"
+import Layout from "./routes/layout/layout";
+import ListPage from "./routes/listPage/listPage";
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+import SinglePage from "./routes/singlePage/singlePage";
+import ProfilePage from "./routes/profilePage/profilePage";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+      element: <HomePage />
+        },
+        {
+          path: "/list",
+      element: <ListPage />
+        },
+        {
+          path: "/:id",
+      element: <SinglePage />
+        },
+        {
+          path: "/profile",
+      element: <ProfilePage />
+        }
+      ]
+    }
+  ]);
+
+
+
   return (
-    <div>Hello World</div>
-  )
+   
+
+      <RouterProvider  router={router}/>
+  );
 }
 
 export default App
